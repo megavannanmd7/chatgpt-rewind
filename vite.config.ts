058008c5060a649624db243b76dcd87f709b3c84
Megavannan M D <megavannanmd7@gmail.com>
2025-12-06 16:39:01 +0530

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chatgpt-rewind-25/",  
+  base: "/chatgpt-rewind/",  
 })
